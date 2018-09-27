aeeaadb732f415c0f1dacd7f558dec56427ce2dd
Humberto <humberto.castro@mira.vc>
2018-09-27 14:52:30 -0300

    sliding window an fixed finished

--- project/machinelearning.py
+++ project/machinelearning.py
@@ -5,15 +5,6 @@ features1dayAhead = ['open-2', 'high-2', 'low-2', 'close-2','close-0']
 features5daysAhead = ['open-7','open-6','high-7','high-6', 'low-7','low-6', 'close-7','close-6', 'close-0']
 features22daysAhead = ['open-24','open-23','high-24','high-23', 'low-24','low-23', 'close-24','close-23', 'close-0']
 
-def executeSVRForDaysAhead(daysAhead, dataset, sizeDataset, trainPercent):
-	if daysAhead == 1:
-		return executeSVR(features=features1dayAhead, originalDataset=dataset, sizeDataset=sizeDataset, trainPercent=trainPercent)
-	elif daysAhead == 5:
-		return executeSVR(features=features5daysAhead, originalDataset=dataset, sizeDataset=sizeDataset, trainPercent=trainPercent)
-	elif daysAhead == 22:
-		return executeSVR(features=features22daysAhead, originalDataset=dataset, sizeDataset=sizeDataset, trainPercent=trainPercent)
-	else:
-		print("invalid svr date")
 
 def executeSVR(features, originalDataset, sizeDataset=550, trainPercent=0.7):
 
@@ -48,3 +39,83 @@ def executeSVR(features, originalDataset, sizeDataset=550, trainPercent=0.7):
 	data = {'date': testDataset.index, 'real': y_original_values, 'predicted': y_predicted_values}
 	dataFrameCompared = pd.DataFrame(data=data)
 	return dataFrameCompared
+
+
+def executeSVRSlidingWindowing(features, originalDataset, sizeDataset=550, trainPercent=0.7):
+	originalDataset = originalDataset[:sizeDataset]
+	transformedDataset = originalDataset[features]
+
+	lengthDataset = len(transformedDataset)
+	trainLength = int(lengthDataset * trainPercent)
+	testLength = lengthDataset - trainLength 
+
+	# Separando dados de entrada e de saída
+	inputFeatures = features[:-1] # Get features from the first to one before the last
+	outputFeatures = features[-1] # Get the last one on array
+
+
+	y_original_values = []
+	y_predicted_values = []
+
+	# Train dataset initial + 1 finish + 1
+	# test datase initial + 1 finish = length
+	for offset in range(0, testLength):
+		trainDataset = transformedDataset[offset:trainLength+offset]
+		testDataset = transformedDataset[offset+trainLength:lengthDataset]
+
+		x_input = trainDataset[inputFeatures]
+		y_output = trainDataset[outputFeatures].values.ravel()
+
+		# Treinando dados de entrada 
+		svr_rbf = SVR(kernel='rbf')#, verbose=True)
+		trainedModel = svr_rbf.fit(x_input, y_output)
+
+		# Separando os dados de entrada e os de saída
+		x_input_test = testDataset[inputFeatures]
+		y_original_value = testDataset[outputFeatures].values.ravel()[0]
+
+		# Prediz valores do dataset de teste
+		y_predicted_value = trainedModel.predict(x_input_test)[0]
+		print('trainSize go from: ' + str(offset) + ' to ' + str(trainLength+offset))
+		print('testSize go from: ' + str(offset+trainLength) + ' to ' + str(lengthDataset))
+		print('offset: ' + str(offset) + ' --- predict:' + str(y_predicted_value) + ' --- original:' + str(y_original_value))
+		
+		y_original_values.append(y_original_value)
+		y_predicted_values.append(y_predicted_value)
+
+	
+	testDataset = transformedDataset[trainLength:lengthDataset]
+	data = {'date': testDataset.index, 'real': y_original_values, 'predicted': y_predicted_values}
+	dataFrameCompared = pd.DataFrame(data=data)
+
+	return dataFrameCompared
+
+
+
+def executeSVRForDaysAhead(daysAhead, dataset, sizeDataset, trainPercent, isSlidingWindow=False):
+	if isSlidingWindow == True:
+		if daysAhead == 1:
+			return executeSVRSlidingWindowing(features=features1dayAhead, originalDataset=dataset, sizeDataset=sizeDataset, trainPercent=trainPercent)
+		elif daysAhead == 5:
+			return executeSVRSlidingWindowing(features=features5daysAhead, originalDataset=dataset, sizeDataset=sizeDataset, trainPercent=trainPercent)
+		elif daysAhead == 22:
+			return executeSVRSlidingWindowing(features=features22daysAhead, originalDataset=dataset, sizeDataset=sizeDataset, trainPercent=trainPercent)
+		else:
+			print("invalid svr date")
+	else:
+		if daysAhead == 1:
+			return executeSVR(features=features1dayAhead, originalDataset=dataset, sizeDataset=sizeDataset, trainPercent=trainPercent)
+		elif daysAhead == 5:
+			return executeSVR(features=features5daysAhead, originalDataset=dataset, sizeDataset=sizeDataset, trainPercent=trainPercent)
+		elif daysAhead == 22:
+			return executeSVR(features=features22daysAhead, originalDataset=dataset, sizeDataset=sizeDataset, trainPercent=trainPercent)
+		else:
+			print("invalid svr date")
+
+
+
+
+
+
+
+
--- project/main.py
+++ project/main.py
@@ -7,26 +7,53 @@ import numpy as np
 from sklearn.metrics import mean_squared_error
 
 
-def results(stockName, filename, daysAhead, rangeSizeDataset, rangeTrainPercent):
-	originalDataset = files.getDataset(filename='../original-data/' + filename)
-	preprocessedDataset = ppc.preprocessing(daysAhead=daysAhead, dataset=originalDataset)
+# def results(stockName, filename, daysAhead, rangeSizeDataset, rangeTrainPercent):
+# 	originalDataset = files.getDataset(filename='../original-data/' + filename)
+# 	preprocessedDataset = ppc.preprocessing(daysAhead=daysAhead, dataset=originalDataset)
 
-	infos = []
-	for sizeDataset in rangeSizeDataset:
-		for trainPercent in rangeTrainPercent:
-			dataFrameCompared = ml.executeSVRForDaysAhead(daysAhead=daysAhead, dataset=preprocessedDataset, sizeDataset=sizeDataset, trainPercent=trainPercent)
-			mape = er.mean_absolute_percentage_error(dataFrameCompared.real, dataFrameCompared.predicted)
-			mse = mean_squared_error(dataFrameCompared.real, dataFrameCompared.predicted) 
-			print(str(sizeDataset) +';'+ str(trainPercent) + ';' + str(mse) + ';' + str(mape) + ';')
-			infos.append(files.DataInfo(sizeDataset=sizeDataset, trainPercent=trainPercent, mse=mse, mape=mape))
+# 	infos = []
 
-	files.csvFile(name=stockName, rowsInfo=infos)
+# 	for sizeDataset in rangeSizeDataset:
+# 		for trainPercent in rangeTrainPercent:
+# 			dataFrameCompared = ml.executeSVRForDaysAhead(daysAhead=daysAhead, dataset=preprocessedDataset, sizeDataset=sizeDataset, trainPercent=trainPercent)
+# 			mape = er.mean_absolute_percentage_error(dataFrameCompared.real, dataFrameCompared.predicted)
+# 			mse = mean_squared_error(dataFrameCompared.real, dataFrameCompared.predicted) 
+# 			print(str(sizeDataset) +';'+ str(trainPercent) + ';' + str(mse) + ';' + str(mape) + ';')
+# 			infos.append(files.DataInfo(sizeDataset=sizeDataset, trainPercent=trainPercent, mse=mse, mape=mape))
 
+# 	files.csvFile(name=stockName, rowsInfo=infos)
+
+def fixedWindow(dataset, daysAhead=1, sizeDataset=550, trainPercent=0.7):
+	dataFrameCompared = ml.executeSVRForDaysAhead(isSlidingWindow=False, daysAhead=daysAhead, dataset=dataset, sizeDataset=sizeDataset, trainPercent=trainPercent)
+	mape = er.mean_absolute_percentage_error(dataFrameCompared.real, dataFrameCompared.predicted)
+	mse = mean_squared_error(dataFrameCompared.real, dataFrameCompared.predicted) 
+	print(str(sizeDataset) +';'+ str(trainPercent) + ';' + str(mse) + ';' + str(mape) + ';')
+
+	return files.DataInfo(sizeDataset=sizeDataset, trainPercent=trainPercent, mse=mse, mape=mape)
+
+def slidingWindow(dataset, daysAhead=1, sizeDataset=550, trainPercent=0.7):
+	dataFrameCompared = ml.executeSVRForDaysAhead(isSlidingWindow=True, daysAhead=daysAhead, dataset=dataset, sizeDataset=sizeDataset, trainPercent=trainPercent)
+	mape = er.mean_absolute_percentage_error(dataFrameCompared.real, dataFrameCompared.predicted)
+	mse = mean_squared_error(dataFrameCompared.real, dataFrameCompared.predicted) 
+	#pl.plot(dataFrameCompared=dataFrameCompared)
+	print(str(sizeDataset) +';'+ str(trainPercent) + ';' + str(mse) + ';' + str(mape) + ';')
+
+	return files.DataInfo(sizeDataset=sizeDataset, trainPercent=trainPercent, mse=mse, mape=mape)
 
 
 def main():
-	results(stockName='itsa4', filename='ITSA4-20140919-20180919.csv', daysAhead=1, 
-		rangeSizeDataset=range(50,55), rangeTrainPercent=np.arange(0.1, 0.9, 0.01))
+	daysAhead = 1
+	sizeDataset = 550
+	trainPercent = 0.7
+
+	originalDataset = files.getDataset(filename='../original-data/' + 'ITSA4-20140919-20180919.csv')
+	preprocessedDataset = ppc.preprocessing(daysAhead=daysAhead, dataset=originalDataset)
+
+	slidingWindow(dataset=preprocessedDataset, daysAhead=daysAhead, sizeDataset=sizeDataset, trainPercent=trainPercent)
+	fixedWindow(dataset=preprocessedDataset, daysAhead=daysAhead, sizeDataset=sizeDataset, trainPercent=trainPercent)
+
+	#results(stockName='itsa4', filename='ITSA4-20140919-20180919.csv', daysAhead=1, 
+	#	rangeSizeDataset=range(550,551), rangeTrainPercent=np.arange(0.7, 0.71, 0.01))
 
 	#pl.plot(dataFrameCompared=dataFrameCompared)
 
