c80bbd12ed15a9d54106ebcef436b5868f7c335d
Humberto <humberto.castro@mira.vc>
2018-09-27 17:54:17 -0300

    globalizing results function

--- project/main.py
+++ project/main.py
@@ -7,30 +7,41 @@ import numpy as np
 import windowing as wdw
 import results as rs
 
-def results():
+def allResults():
 	originalDataset = files.getDataset(filename='../original-data/' + 'ITSA4-20140919-20180919.csv')
 
 	preprocessedDataset1Day = ppc.preprocessing(daysAhead=1, dataset=originalDataset)
 	preprocessedDataset5Days = ppc.preprocessing(daysAhead=5, dataset=originalDataset)
 	preprocessedDataset22Days = ppc.preprocessing(daysAhead=22, dataset=originalDataset)
+	stockName = 'itsa4'
+
 
 	print('------ FIXED WINDOW 1 DAY -------')
-	rs.getResultsFixedWindow(stockName='itsa4', dataset=preprocessedDataset1Day, daysAhead=1, 
-		rangeSizeDataset=range(50,1000), rangeTrainPercent=np.arange(0.5, 0.90, 0.01))
+	rs.getResultsFixedWindow(stockName=stockName, dataset=preprocessedDataset1Day, daysAhead=1, 
+		rangeSizeDataset=range(50,51), rangeTrainPercent=np.arange(0.5, 0.90, 0.01))
 
 	print('------ FIXED WINDOW 5 DAYS -------')
-	rs.getResultsFixedWindow(stockName='itsa4', dataset=preprocessedDataset5Days, daysAhead=5, 
-		rangeSizeDataset=range(50,1000), rangeTrainPercent=np.arange(0.5, 0.90, 0.01))
+	rs.getResultsFixedWindow(stockName=stockName, dataset=preprocessedDataset5Days, daysAhead=5, 
+		rangeSizeDataset=range(50,51), rangeTrainPercent=np.arange(0.5, 0.90, 0.01))
+
+	print('------ FIXED WINDOW 22 DAYS -------')
+	rs.getResultsFixedWindow(stockName=stockName, dataset=preprocessedDataset22Days, daysAhead=22, 
+		rangeSizeDataset=range(50,51), rangeTrainPercent=np.arange(0.5, 0.90, 0.01))
+
 
 	print('------ SLIDING WINDOW 1 DAY -------')
-	rs.getResultsSlidingWindow(stockName='itsa4', dataset=preprocessedDataset1Day, daysAhead=1, 
-		rangeSizeDataset=range(50,1000), rangeTrainPercent=np.arange(0.5, 0.90, 0.01))
+	rs.getResultsSlidingWindow(stockName=stockName, dataset=preprocessedDataset1Day, daysAhead=1, 
+		rangeSizeDataset=range(50,51), rangeTrainPercent=np.arange(0.5, 0.90, 0.01))
 
 	print('------ SLIDING WINDOW 5 DAYS -------')
-	rs.getResultsSlidingWindow(stockName='itsa4', dataset=preprocessedDataset5Days, daysAhead=5, 
-		rangeSizeDataset=range(50,1000), rangeTrainPercent=np.arange(0.5, 0.90, 0.01))
+	rs.getResultsSlidingWindow(stockName=stockName, dataset=preprocessedDataset5Days, daysAhead=5, 
+		rangeSizeDataset=range(50,51), rangeTrainPercent=np.arange(0.5, 0.90, 0.01))
 
-def main():
+	print('------ SLIDING WINDOW 22 DAYS -------')
+	rs.getResultsSlidingWindow(stockName=stockName, dataset=preprocessedDataset22Days, daysAhead=22, 
+		rangeSizeDataset=range(50,51), rangeTrainPercent=np.arange(0.5, 0.90, 0.01))
+
+def oneResult():
 	daysAhead = 1
 	sizeDataset = 550
 	trainPercent = 0.7
@@ -39,11 +50,11 @@ def main():
 	preprocessedDataset = ppc.preprocessing(daysAhead=daysAhead, dataset=originalDataset)
 	
 	dataInfoSlidingWindow = wdw.slidingWindow(dataset=preprocessedDataset, daysAhead=daysAhead, sizeDataset=sizeDataset, trainPercent=trainPercent)
-	#dataInfoFixedWindow = wdw.fixedWindow(dataset=preprocessedDataset, daysAhead=daysAhead, sizeDataset=sizeDataset, trainPercent=trainPercent)
-
-	#pl.plot(dataFrameCompared=dataInfoSlidingWindow.dataFrameCompared)
-	#pl.plot(dataFrameCompared=dataInfoFixedWindow.dataFrameCompared)
 	pl.plot(dataFrameCompared=dataInfoSlidingWindow.dataFrameCompared, title='REAL vs Predito')
+
+def main():
+	#oneResult()
+	allResults()
 
 main()
 
